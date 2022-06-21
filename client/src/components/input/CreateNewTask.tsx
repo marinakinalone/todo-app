21316f9ac4c09cdb99514ab1e5aa456da48fa9a3
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-06-21 21:28:46 +0200

    add styling to CreateNewTask component

--- client/src/components/input/CreateNewTask.tsx
+++ client/src/components/input/CreateNewTask.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const CreateNewTask = ({valueState, setValueState, handleSubmit}: any) => {
   return (
-    <section className="task__create">
+    <section className="todos__create">
         <input required={true}
                 type="text"
                 name="text"
-                className="task__create-input"
+                className="todos__create-input"
                 onKeyPress={handleSubmit}
                 value={valueState}
                 onChange={(e) => setValueState(e.target.value)} />
