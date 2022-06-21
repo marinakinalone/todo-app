45ca68d8ea3f49c469fa4ea2c7763c6db81bec58
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-06-21 10:58:46 +0200

    implement create a new todo list

--- client/src/components/Summary.tsx
+++ client/src/components/Summary.tsx
@@ -6,6 +6,8 @@ import { TodoList } from '../ts-utils/types';
 const Summary = () => {
     const [todoListNames, setTodoListNames] = useState<Array<TodoList>>([])
     const [loading, setLoading] = useState(true);
+    const [valueState, setValueState] = useState("");
+
     useEffect(() => {
         const fetchListNames = async () => {
             const data = await fetch("https://tout-doux-server.herokuapp.com/lists/all")
@@ -18,6 +20,24 @@ const Summary = () => {
         console.log(todoListNames)
         fetchListNames();
     }, [loading])
+
+    const handleSubmit = async (e:React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && valueState !== "") {
+            console.log(valueState)
+            await fetch('https://tout-doux-server.herokuapp.com/lists/create', {
+                method: 'POST', 
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({"name": valueState})
+            })
+            const data = await fetch("https://tout-doux-server.herokuapp.com/lists/all")
+            const result = await data.json();
+            setTodoListNames([...result])
+            setValueState('')
+        }
+    }
 
     return (
         <main className="page__todos">
@@ -36,7 +56,13 @@ const Summary = () => {
                         })}
                     </ul>
                     <section className="todos__create">
-                        <input required={true} type="text" name="text" className="todos__create-input" />
+                        <input required={true}
+                        type="text"
+                        name="text"
+                        className="todos__create-input"
+                        onKeyPress={handleSubmit}
+                        value={valueState}
+                        onChange={ (e) => setValueState(e.target.value) } />
                         <label className="todos__create-label">create a new list</label>
                     </section>
                 </>
