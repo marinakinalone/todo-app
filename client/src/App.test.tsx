3e174e1a2b0dcbb29b1ec5c367187f24994eaff5
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-06-23 13:27:56 +0200

    write a snapshot test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('renders all the components', () => {
+  test('snapshot test for App', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 });
